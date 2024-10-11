6261e141c09ca7383bda9a6ac4e173d13c918251
Ignacio Frizzera <ignaciofrizzera@gmail.com>
2024-10-11 17:09:28 -0300

    development: allow only GET method for the endpoint

--- berries/views.py
+++ berries/views.py
@@ -1,6 +1,10 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 from django.http.request import HttpRequest
 from django.http import HttpResponse
 
 
+@csrf_exempt
+@require_http_methods(["GET"])
 def all_berry_stats(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Hello Word', status=200)
