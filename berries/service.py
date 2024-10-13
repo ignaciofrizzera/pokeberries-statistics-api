ac5b63ad65c9cde60452d936b29acc2db962d828
Ignacio Frizzera <ignaciofrizzera@gmail.com>
2024-10-13 18:33:13 -0300

    visualization: bins histogram done

--- berries/service.py
+++ berries/service.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Any, Union, Tuple
 from collections import Counter
 import matplotlib
 matplotlib.use('Agg')
+from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import io
 import base64
@@ -66,15 +67,7 @@ class BerriesService:
         return statistics_data, growth_times
     
     @staticmethod
-    def __generate_bar_chart_image(labels: List[Any], values: List[Any], title: str, x_label: str, y_label: str) -> str:
-        _, ax = plt.subplots()
-        ax.set_title(title)
-        ax.set_xlabel(x_label)
-        ax.set_ylabel(y_label)
-        ax.set_xticks(labels)
-        ax.set_yticks(range(0, max(values) + 1))
-        ax.bar(labels, values)
-
+    def __encode_plot() -> str:
         buffer = io.BytesIO()
         plt.savefig(buffer, format='png')
         buffer.seek(0)
@@ -82,6 +75,26 @@ class BerriesService:
         buffer.close()
 
         return base64.b64encode(image_png).decode('utf-8')
+    
+    @staticmethod
+    def __setup_plot(title: str, x_label: str, y_label: str) -> Axes:
+        _, ax = plt.subplots()
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        return ax
+    
+    def __generate_bar_chart_image(self, labels: List[Any], values: List[Any], title: str, x_label: str, y_label: str) -> str:
+        ax = self.__setup_plot(title, x_label, y_label)
+        ax.set_xticks(labels)
+        ax.set_yticks(range(0, max(values) + 1))
+        ax.bar(labels, values)
+        return self.__encode_plot()
+        
+    def __generate_bins_histogram_image(self, values: List[int], bins: int, title: str, x_label: str, y_label: str) -> str:
+        ax = self.__setup_plot(title, x_label, y_label)
+        ax.hist(values, bins=bins, rwidth=0.8)
+        return self.__encode_plot()
     
     def get_data_for_visualization(self) -> Dict[str, Any]:
         statistics_data, growth_times = self.get_statistics(for_visualization=True)
@@ -91,5 +104,10 @@ class BerriesService:
         bar_chart_image = self.__generate_bar_chart_image(
             labels, values, title="Growth Times among the Berries", x_label="Growth Time", y_label="Frequency")
         
-        statistics_data["image"] = bar_chart_image
+        bins_histogram_image = self.__generate_bins_histogram_image(
+            growth_times, bins=5, title="Bins of Growth Times", x_label="Bins", y_label="Growth Time")
+
+        statistics_data["bar_chart"] = bar_chart_image
+        statistics_data["bins_histogram"] = bins_histogram_image
+        
         return statistics_data
