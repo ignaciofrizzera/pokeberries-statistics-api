2b5e1d12c7c90dfce1046d012a21b011c220bf2e
Ignacio Frizzera <ignaciofrizzera@gmail.com>
2024-10-13 13:11:58 -0300

    visualization: base setup to visualize the berries data.

--- berries/views.py
+++ berries/views.py
@@ -1,8 +1,10 @@
 from berries.service import BerriesService
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
 from django.http.request import HttpRequest
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from typing import Union
 
 
 @csrf_exempt
@@ -13,3 +15,12 @@ def all_berry_stats(request: HttpRequest) -> JsonResponse:
         return JsonResponse(berry_stats, status=200)
     except Exception:
         return JsonResponse({'error': 'There was an error processing the berry statistics.'}, status=500)
+
+@csrf_exempt
+@require_http_methods(["GET"])
+def berries_stats_visualization(request: HttpRequest) -> Union[HttpResponse, JsonResponse]:
+    # try:
+    berry_stats = BerriesService().get_statistics()
+    return render(request, template_name='berries_stats_visualization.html', context=berry_stats)
+    # except Exception:
+        # return JsonResponse({'error': 'There was an error generating the visualization for the berries data.'}, status=500)
